069179cec63326cf89b2d8b050ad253a7cbd6824
afroshyi <afrozakram11@gmail.com>
2024-11-12 05:42:59 -0500

    uh we lost so much progress

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,28 @@ router.get('/home', function(req, res, next) {
     title: 'Home' });
 });
 
+/* GET Apointments page. */
+router.get('/appointments', function(req, res, next) {
+  res.render('appointments', { 
+    title: 'Book an Appointment'
+  });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { 
+    title: 'Contact Us'
+  });
+});
+router.post('/appointments', function(req, res, next) {
+  const appointmentData = req.body;
+  console.log(appointmentData);
+  res.render('appointment-confirmation', { 
+    title: 'Appointment Confirmed',
+    appointment: appointmentData
+  });
+});
+
+
 
 
 module.exports = router;
